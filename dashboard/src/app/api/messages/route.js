08552b7dcd350d9890ee0256fb65f8e39ae90e82
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2026-05-21 09:28:12 -0300

    feat: remove web ui

--- dashboard/src/app/api/messages/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import generateFakeMessages from "../../data/messages.js";
-
-const data = generateFakeMessages(20);
-
-export async function GET(request, { params }) {
-  return NextResponse.json(data);
-}
--- dashboard/src/app/page.js
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import Dashboard from "../components/ui/dashboard";
-
-export default function Home() {
-  return <div></div>;
-}
